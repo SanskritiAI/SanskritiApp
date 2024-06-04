ed4ee6cd720454644f13a8f2e706559704f2afd2
Anindyadeep <proanindyadeep@gmail.com>
2024-06-04 23:26:48 +0530

    updated guidelines

--- sanskriti_bench/components/guidelines.py
+++ sanskriti_bench/components/guidelines.py
@@ -1,35 +1,48 @@
 import streamlit as st 
 
-markdown_body = """
-### Rules
-- Reach out to <@693870619967881244> <@773807047866449933> for participating in your language groups, becoming language managers, mentors or sponsors 
-- Each language groups are private channels
-- Every Week on Friday, we will have a catch-up to understand the progress. Each Language Manager will share the status of their language
-- In case your language is not currently being worked upon, pls reach out so that we can start the progress
+BODY = """
+
+## Guidelines
+We expect you to follow these guidelines when you start contributing to this project. 
+
+### General rules
+
+- It would be best if you join the discord [server](https://discord.gg/rGyyDjEgzS) and request in the #general chat discussion regarding which group you want to contribute to and what role you are interested in. 
+
+- After that we will message you on discord with a user_id and password which you will use to login in this platform and start contributing. Please make sure that you do not share this password to anyone else. 
+
+- If you are not comfortable with discord messaging platform then kindly mail us at `proanindyadeep@gmail.com` or `guneetsk99@gmail.com` for enquiring the same. 
+
+- If you apply as a `contributor` role, then you need to just log in to the platform and contribute as many examples as possible. You need to provide the question and answer in your indic / native language. Please make sure the answer you put is correct. 
+
 - Be kind to all. It's an open science; our work is for the Indian Language and our cultures. 
+
 - Mentors would be available for any help related to data building
-### Rules for Data Construction
 
-### Nature of Questions:
-- Questions should blend subjective and factual elements, avoiding purely factual queries.
-- Avoid direct "what" questions (e.g., "What is this?" or "What is that?").
+- All the contributor's contribution will be mentioned and shown publicly in the hall of fame once the project is completed. Contributors with
+most numbers of contributions will be rewarded with some incentive. 
 
-### Examples to Avoid:
-- Do not create overly general knowledge questions (e.g., "What is the capital of India?").
-   - The general knowledge questions that are very native to your region or state will still be better to focus on
-- Avoid questions related to personal biases and views.
-- Refrain from questions related to recent politics (post-2000).
+### How you should frame the question:
 
-### Content Guidelines:
-- Slang is allowed but should be used constructively and contextually.
-- Questions can be related to daily lives and relatable scenarios.
-- You can include questions about your culture and history, ensuring you know the answers.
+Here are some rules and suggestions on how you should make different questions:
 
-### Contribution Rules:
-- Do not repeat or copy questions from others. Each question should be unique.
-- This is a community effort, not a competition. There is no pressure to compete.
+1. DO NOT include any topic which are super controversial or very political in nature. Like asking for opinions etc. You can alaways ask 
+questions related to political facts or anything related to history. Refrain from questions related to recent politics (post-2000).
+
+2. Please DO NOT question super generic question. For example: What is the captical of India or Who created Taj Mahal? etc etc. Ask questions
+which are related to your culture and questions which are not often asked for daily lives. Example: Question related to Durga Puja, when language is bengali, because it is an important cultural part for Bengal. 
+
+3. It would be GREAT if you ask questions starting with a negation or the answer should start with No. For example: Instead of asking
+Is Onam celebrated on Kerala?, ask this question: Is Onam not celebrated on Kerala? 
+
+4. Always ask questions using your indic / native language. 
+
+5. Do not repeat or copy questions from others. Each question should be unique. Please remmember this is not a competition. There is 
+absolutely no pressure. 
+
+6. Avoid questions related to personal biases and views.
 """
 
 
 def guidelines():
-    st.markdown(markdown_body)
+    st.markdown(BODY)
